b580e262bd951ebd53914352dd034411237e5ce3
Justin Hauville <jhauvill@c1r4p11.42lehavre.fr>
2026-03-18 13:02:07 +0100

    cpp01 ex02

---
+++ CPP01/ex02/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+	std::string string = "HI THIS IS BRAIN";
+	std::string* stringPTR = &string;
+	std::string& stringREF = string;
+
+	std::cout << "Memory Adress:" << std::endl;
+	std::cout << "string " << &string << std::endl;
+	std::cout << "stringPTR " << stringPTR << std::endl;
+	std::cout << "stringREF " << &stringREF << std::endl;
+
+	std::cout << "Value:" << std::endl;
+	std::cout << "string " << string << std::endl;
+	std::cout << "stringPTR " << *stringPTR << std::endl;
+	std::cout << "stringREF " << stringREF << std::endl;
+	return 0;
+}
