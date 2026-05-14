70d2f27251c1d412287ab9c76be854c8bb2cefba
Justin Hauville <jhauvill@c1r4p11.42lehavre.fr>
2026-05-14 16:30:42 +0200

    cpp06 ex02

---
+++ CPP06/ex02/Base.hpp
@@ -0,0 +1,14 @@
+#ifndef BASE_HPP
+#define BASE_HPP
+
+class Base
+{
+private:
+
+public:
+	Base();
+	virtual ~Base();
+};
+
+
+#endif
---
+++ CPP06/ex02/main.cpp
@@ -0,0 +1,73 @@
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+#include <cstdlib>
+#include <iostream>
+#include <exception>
+
+Base *generate(void)
+{
+	srand(time(NULL));
+	int random = rand() % 3;
+	if (!random)
+		return (new A);
+	else if (random == 1)
+		return (new B);
+	else
+		return (new C);
+}
+
+void identify(Base *p)
+{
+	if (dynamic_cast<A*>(p))
+		std::cout << "A" << std::endl;
+	else if (dynamic_cast<B*>(p))
+		std::cout << "B" << std::endl;
+	else
+		std::cout << "C" << std::endl;
+}
+
+void identify(Base &p)
+{
+	try
+	{
+		A &a = dynamic_cast<A&>(p);
+		(void)a;
+		std::cout << "A" << std::endl;
+	}
+	catch(const std::exception &e)
+	{
+		(void)e;
+	}
+	
+	try
+	{
+		B &b = dynamic_cast<B&>(p);
+		(void)b;
+		std::cout << "B" << std::endl;
+	}
+	catch(const std::exception &e)
+	{
+		(void)e;
+	}
+	
+	try
+	{
+		C &c = dynamic_cast<C&>(p);
+		(void)c;
+		std::cout << "C" << std::endl;
+	}
+	catch(const std::exception &e)
+	{
+		(void)e;
+	}
+}
+
+int main()
+{
+	Base *ptr = generate();
+	identify(ptr);
+	identify(*ptr);
+	delete ptr;
+	return 0;
+}
